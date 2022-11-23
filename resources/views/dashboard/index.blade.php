8964d10d7fd2ae5c471cb7b10fa0306af14f88f7
Juan Garcia <ichsanugraha@gmail.com>
2022-11-23 15:58:37 +0700

    views changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,7 +25,7 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Order <span>| Today</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -60,7 +60,7 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Total Purchase <span>| This Month</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
